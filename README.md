3b13baafff00b2a60c2e91d0ca599cf3ea7b6374
Paul Clifford <paul.clifford@gmail.com>
2012-10-07 10:19:05 +0100

    Proof of concept

---
+++ README.md
@@ -0,0 +1,33 @@
+A simple command line utility to extract player information from a Borderlands
+2 save file, or to create a new save file from player information.
+
+Note the following before trying to use it:
+
+* It has no graphical interface and is not easy to use
+* It does not provide any mechanisms for creating or modifying items and weapons
+* It is a proof of concept and will corrupt your save files if used improperly
+* It requires a working Python 2 interpreter (2.6 or later) and the Python LZO
+  bindings.
+
+Examples:
+
+Extract the raw protobuf data from a save file:
+
+    ./savefile.py -d < your-save-game.sav > player.p
+
+Create a new save file from protobuf data:
+
+    ./savefile.py < player.p > your-new-save-game.sav
+
+Extract the data in JSON format (encoded to allow round-tripping):
+
+    ./savefile.py -d -j < your-save-game.sav > player.json
+
+Extract the data in JSON format, applying further protobuf decoding to the
+listed fields:
+
+    ./savefile.py -d -j -p 6:0,8,11,13,18,19,29,30,34,38,53,54 < your-save-game.sav > player.json
+
+Create a new save file from the JSON data:
+
+    ./savefile.py -j < player.json > your-new-save-game.sav
---
+++ savefile.py
@@ -0,0 +1,355 @@
+#! /usr/bin/env python
+
+from bisect import insort
+from cStringIO import StringIO
+import hashlib
+import json
+import optparse
+import struct
+import sys
+
+import lzo
+
+
+class BL2Error(Exception): pass
+
+
+class ReadBitstream(object):
+
+    def __init__(self, s):
+        self.s = s
+        self.i = 0
+
+    def read_bit(self):
+        i = self.i
+        self.i = i + 1
+        byte = ord(self.s[i >> 3])
+        bit = byte >> (7 - (i & 7))
+        return bit & 1
+
+    def read_bits(self, n):
+        s = self.s
+        i = self.i
+        end = i + n
+        chunk = s[i >> 3: (end + 7) >> 3]
+        value = ord(chunk[0]) &~ (0xff00 >> (i & 7))
+        for c in chunk[1: ]:
+            value = (value << 8) | ord(c)
+        if (end & 7) != 0:
+            value = value >> (8 - (end & 7))
+        self.i = end
+        return value
+
+    def read_byte(self):
+        i = self.i
+        self.i = i + 8
+        byte = ord(self.s[i >> 3])
+        if (i & 7) == 0:
+            return byte
+        byte = (byte << 8) | ord(self.s[(i >> 3) + 1])
+        return (byte >> (8 - (i & 7))) & 0xff
+
+    def read_word(self):
+        i = self.i
+        if (i & 7) == 0:
+            self.i = i + 32
+            return struct.unpack(">I", self.s[i: i + 4])[0]
+        else:
+            w = (self.read_byte() << 24) | \
+                (self.read_byte() << 16) | \
+                (self.read_byte() <<  8) | \
+                self.read_byte()
+            return w
+
+class WriteBitstream(object):
+
+    def __init__(self):
+        self.s = ""
+        self.byte = 0
+        self.i = 7
+
+    def write_bit(self, b):
+        i = self.i
+        byte = self.byte | (b << i)
+        if i == 0:
+            self.s += chr(byte)
+            self.byte = 0
+            self.i = 7
+        else:
+            self.byte = byte
+            self.i = i - 1
+
+    def write_bits(self, b, n):
+        s = self.s
+        byte = self.byte
+        i = self.i
+        while n >= (i + 1):
+            shift = n - (i + 1)
+            n = n - (i + 1)
+            byte = byte | (b >> shift)
+            b = b &~ (byte << shift)
+            s = s + chr(byte)
+            byte = 0
+            i = 7
+        if n > 0:
+            byte = byte | (b << (i + 1 - n))
+            i = i - n
+        self.s = s
+        self.byte = byte
+        self.i = i
+
+    def write_byte(self, b):
+        i = self.i
+        if i == 7:
+            self.s += chr(b)
+        else:
+            self.s += chr(self.byte | (b >> (7 - i)))
+            self.byte = (b << (i + 1)) & 0xff
+
+    def getvalue(self):
+        if self.i != 7:
+            return self.s + chr(self.byte)
+        else:
+            return self.s
+
+
+def read_huffman_tree(b):
+    node_type = b.read_bit()
+    if node_type == 0:
+        return (None, (read_huffman_tree(b), read_huffman_tree(b)))
+    else:
+        return (None, b.read_byte())
+
+def write_huffman_tree(node, b):
+    if type(node[1]) is int:
+        b.write_bit(1)
+        b.write_byte(node[1])
+    else:
+        b.write_bit(0)
+        write_huffman_tree(node[1][0], b)
+        write_huffman_tree(node[1][1], b)
+
+def make_huffman_tree(data):
+    frequencies = [0] * 256
+    for c in data:
+        frequencies[ord(c)] += 1
+
+    nodes = [[f, i] for (i, f) in enumerate(frequencies) if f != 0]
+    nodes.sort()
+
+    while len(nodes) > 1:
+        l, r = nodes[: 2]
+        nodes = nodes[2: ]
+        insort(nodes, [l[0] + r[0], [l, r]])
+
+    return nodes[0]
+
+def invert_tree(node, code=0, bits=0):
+    if type(node[1]) is int:
+        return {chr(node[1]): (code, bits)}
+    else:
+        d = {}
+        d.update(invert_tree(node[1][0], code << 1, bits + 1))
+        d.update(invert_tree(node[1][1], (code << 1) | 1, bits + 1))
+        return d
+
+def huffman_decompress(tree, bitstream, size):
+    output = ""
+    while len(output) < size:
+        node = tree
+        while 1:
+            b = bitstream.read_bit()
+            node = node[1][b]
+            if type(node[1]) is int:
+                output += chr(node[1])
+                break
+    return output
+
+def huffman_compress(encoding, data, bitstream):
+    for c in data:
+        code, nbits = encoding[c]
+        bitstream.write_bits(code, nbits)
+
+
+def read_varint(f):
+    value = 0
+    offset = 0
+    while 1:
+        b = ord(f.read(1))
+        value |= (b & 0x7f) << offset
+        if (b & 0x80) == 0:
+            break
+        offset = offset + 7
+    return value
+
+def write_varint(f, i):
+    while i > 0x7f:
+        f.write(chr(0x80 | (i & 0x7f)))
+        i = i >> 7
+    f.write(chr(i))
+
+def read_protobuf(data):
+    fields = {}
+    end_position = len(data)
+    bytestream = StringIO(data)
+    while bytestream.tell() < end_position:
+        key = read_varint(bytestream)
+        field_number = key >> 3
+        wire_type = key & 7
+        value = read_protobuf_value(bytestream, wire_type)
+        fields.setdefault(field_number, []).append([wire_type, value])
+    return fields
+
+def read_protobuf_value(b, wire_type):
+    if wire_type == 0:
+        value = read_varint(b)
+    elif wire_type == 1:
+        value = struct.unpack("<Q", b.read(8))[0]
+    elif wire_type == 2:
+        length = read_varint(b)
+        value = b.read(length)
+    elif wire_type == 5:
+        value = struct.unpack("<I", b.read(4))[0]
+    else:
+        raise BL2Error("Unsupported wire type " + str(wire_type))
+    return value
+
+def write_protobuf(data):
+    b = StringIO()
+    # If the data came from a JSON file the keys will all be strings
+    data = dict([(int(k), v) for (k, v) in data.items()])
+    for key, entries in sorted(data.items()):
+        for wire_type, value in entries:
+            if type(value) is dict:
+                value = write_protobuf(value)
+                wire_type = 2
+            elif type(value) in (list, tuple):
+                sub_b = StringIO()
+                for v in value:
+                    write_protobuf_value(sub_b, wire_type, v)
+                value = sub_b.getvalue()
+                wire_type = 2
+            write_varint(b, (key << 3) | wire_type)
+            write_protobuf_value(b, wire_type, value)
+    return b.getvalue()
+
+def write_protobuf_value(b, wire_type, value):
+    if wire_type == 0:
+        write_varint(b, value)
+    elif wire_type == 1:
+        b.write(struct.pack("<Q", value))
+    elif wire_type == 2:
+        if type(value) is unicode:
+            value = value.encode("latin1")
+        write_varint(b, len(value))
+        b.write(value)
+    elif wire_type == 5:
+        b.write(struct.pack("<I", value))
+    else:
+        raise BL2Error("Unsupported wire type " + str(wire_type))
+
+def parse_zigzag(i):
+    if i & 1:
+        return -1 ^ (i >> 1)
+    else:
+        return i >> 1
+
+
+def unwrap_player_data(data):
+    if data[: 20] != hashlib.sha1(data[20: ]).digest():
+        raise BL2Error("Invalid save file")
+
+    data = lzo.decompress("\xf0" + data[20: ])
+    size, wsg, version, crc = struct.unpack(">I3sII", data[: 15])
+    if version != 2:
+        raise BL2Error("Unknown save version " + str(version))
+
+    bitstream = ReadBitstream(data[15: ])
+    size = bitstream.read_word()
+    tree = read_huffman_tree(bitstream)
+    player = huffman_decompress(tree, bitstream, size)
+
+    if (lzo.crc32(player) & 0xffffffff) != crc:
+        raise BL2Error("CRC check failed")
+
+    return player
+
+def wrap_player_data(player):
+    bitstream = WriteBitstream()
+
+    tree = make_huffman_tree(player)
+    write_huffman_tree(tree, bitstream)
+    huffman_compress(invert_tree(tree), player, bitstream)
+
+    data = struct.pack(">I", len(player)) + bitstream.getvalue() + "\x00\x00\x00\x00"
+    crc = lzo.crc32(player) & 0xffffffff
+    data = struct.pack(">I3sII", len(data) + 11, "WSG", 2, crc) + data
+    data = lzo.compress(data)[1: ]
+
+    return hashlib.sha1(data).digest() + data
+
+
+def apply_crude_parsing(player, rules):
+    for key in rules.split(","):
+        if ":" in key:
+            key, field_type = key.split(":", 1)
+            field_type = int(field_type)
+            for element in player.get(int(key), []):
+                element[0] = field_type
+                b = StringIO(element[1])
+                end_position = len(element[1])
+                value = []
+                while b.tell() < end_position:
+                    value.append(read_protobuf_value(b, field_type))
+                element[1] = value
+        else:
+            for element in player.get(int(key), []):
+                element[1] = read_protobuf(element[1])
+
+def main():
+    usage = "usage: %prog [options] [source file] [destination file]"
+    p = optparse.OptionParser()
+    p.add_option(
+        "-d", "--decode",
+        action="store_true",
+        help="read from a save game, rather than creating one"
+    )
+    p.add_option(
+        "-j", "--json",
+        action="store_true",
+        help="read or write save game data in JSON format, rather than raw protobufs"
+    )
+    p.add_option(
+        "-p", "--parse",
+        help="perform further protobuf parsing on the specified comma separated list of keys"
+    )
+    options, args = p.parse_args()
+
+    if len(args) < 1 or args[0] == "-":
+        input = sys.stdin
+    else:
+        input = open(args[0], "r")
+
+    if len(args) < 2 or args[1] == "-":
+        output = sys.stdout
+    else:
+        output = open(args[1], "w")
+
+    if options.decode:
+        savegame = input.read()
+        player = unwrap_player_data(savegame)
+        if options.json:
+            player = read_protobuf(player)
+            if options.parse:
+                apply_crude_parsing(player, options.parse)
+            player = json.dumps(player, encoding="latin1", sort_keys=True, indent=4)
+        output.write(player)
+    else:
+        player = input.read()
+        if options.json:
+            player = write_protobuf(json.loads(player, encoding="latin1"))
+        savegame = wrap_player_data(player)
+        output.write(savegame)
+
+if __name__ == "__main__":
+    main()
